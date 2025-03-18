b79b9d3fccaa602580701278e3f8568e07bc5f74
Giorgio Casati <giorgio.casati.afi@gmail.com>
2025-03-18 11:04:40 +0100

    url: getProtocol

--- src/url/index.ts
+++ src/url/index.ts
@@ -3,6 +3,19 @@ export function getRootDomain(request: string | Request | URL) {
     typeof request === 'string' ? new URL(request)
     : request instanceof Request ? new URL(request.url)
     : request
+  // TODO: gestire bug navigando tramite indirizzo IP
   const domainParts = hostname.split('.')
   return domainParts.length > 2 ? domainParts.slice(-2).join('.') : hostname
+}
+
+export function getProtocol(request: string | Request | URL) {
+  if (request instanceof Request) {
+    const fwdProtocol = request.headers.get('x-forwarded-proto')
+    if (fwdProtocol) return fwdProtocol
+
+    return getProtocol(request.url)
+  }
+
+  const url = typeof request === 'string' ? new URL(request) : request
+  return url.protocol
 }
